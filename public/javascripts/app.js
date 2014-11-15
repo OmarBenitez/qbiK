6d3c18346f5bf2a8d40edc0d9fa8f0f638621e7f
Omar Benitez <oabe2911@gmail.com>
2014-11-15 00:10:08 -0700

    Se cambia el template

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,96 +1,74 @@
+var socket = io.connect('http://localhost:1337/');
 
-$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
+angular.module('qbik', []).config(function($routeProvider) {
+    $routeProvider
+            .when('/', {
+                templateUrl: 'public/views/index.html'
+                , controller: 'home'})
+            .when('/detalle/', {
+                templateUrl: 'public/views/detalle.html'
+                , controller: 'detalle'
+            })
+            .when('/add/', {
+                templateUrl: 'public/views/new.html'
+                , controller: 'nuevo'
+            })
+            .otherwise({redirectTo: '/'});
+}).factory('appFactory', function($http, $rootScope) {
 
-    var geocoder = new google.maps.Geocoder();
+    var service = {};
+    service.data = [];
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-    }
-//Get the latitude and the longitude;
-    function successFunction(position) {
-        var lat = position.coords.latitude;
-        var lng = position.coords.longitude;
-        codeLatLng(lat, lng)
+    service.sendEvent = function(event) {
+        $rootScope.$broadcast(event);
     }
 
-    function errorFunction() {
-       
-    }
+    service.load = function() {
+        $http.get('productos.json').success(function(data) {
+            service.data = data;
+            service.sendEvent('productos');
+        })
+    };
 
+    return service;
 
-
-    function codeLatLng(lat, lng) {
-
-        var latlng = new google.maps.LatLng(lat, lng);
-        geocoder.geocode({'latLng': latlng}, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                if (results[1]) {
-                    //formatted address
-                    //find country name
-                    for (var i = 0; i < results[0].address_components.length; i++) {
-                        for (var b = 0; b < results[0].address_components[i].types.length; b++) {
-
-                            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
-                            if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
-                                //this is the object you are looking for
-                                city = results[0].address_components[i];
-                                break;
-                            }
-                        }
-                    }
-                    //city data
-                    var c = document.getElementById('city');
-                    if (c)
-                        c.innerText = city.long_name;
-
-                } else {
-                }
-            } else {
-            }
-        });
-    }
-});
-
-var app = angular.module('qbik', ['textAngular']);
-
-app.controller('mainCtrl', function($scope, $http) {
+}).controller('main', function($scope, $http) {
 
     $scope.estados = [];
     $scope.municipios = [];
-    $scope.estado = null;
-    $scope.municipio = null;
-
-    $http.get('/estados.json').success(function(data, status) {
+    
+    $http.get('/estados.json').success(function(data){
         $scope.estados = data;
     });
-
-    $scope.getMunicipios = function() {
-        $scope.municipio = null;
-        $http.get('/municipios/' + $scope.estado.clave).success(function(data, status) {
+    
+    $scope.getMunicipios = function(){
+        $http.get('/municipios/'+$scope.estado.clave).success(function(data){
             $scope.municipios = data;
         });
     };
 
-    $scope.go = function() {
-
-        window.location = "/publicaciones/" + $scope.municipio.clave;
-
+}).controller('home', function($scope, $rootScope, appFactory) {
+    $scope.prods = [];
+    appFactory.load();
+    $scope.$on('productos', function() {
+        $scope.prods = appFactory.data;
+    });
+    socket.on('updateProds', function(prod) {
+        console.log(prod);
+        $scope.prods.push(prod);
+        $rootScope.$apply();
+    });
+}).controller('detalle', function($scope, appFactory) {
+    $scope.prods = [];
+    appFactory.load();
+    $scope.$on('productos', function() {
+        $scope.prods = appFactory.data;
+    });
+}).controller('nuevo', function($scope, $http) {
+    $scope.send = function(dato) {
+        $http.post('/add/prod', dato).success(function(data, status) {
+            socket.emit('newProd', data);
+            $scope.object = {};
+        });
     };
-
-});
-
-app.controller('newPubCtrl', function($scope) {
-
 });
